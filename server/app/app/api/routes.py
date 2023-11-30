c04b4234e057b1216130ef15f097b94c6a755116
hojzas <hojzas@seznam.cz>
2023-11-30 10:16:12 +0100

    Reorganization of flask project structure (api, uploads, config)

---
+++ server/app/app/api/routes.py
@@ -0,0 +1,27 @@
+from app import app
+from flask import jsonify, request
+from app.uploads.upload_handler import upload_handler
+
+# TODO: delete
+import socket
+
+# TODO: delete
+@app.route("/api/test", methods=['GET', 'POST'])
+def api_upload():
+    return ("host")
+
+# Upload
+@app.route("/api/upload", methods=['GET', 'POST'])
+def upload():
+    return upload_handler(request)
+
+
+
+
+
+
+# TODO: delete
+# Show server
+@app.route("/s", methods=['GET'])
+def return_home():
+    return jsonify({"host": "Hostname: " + socket.gethostname()})
