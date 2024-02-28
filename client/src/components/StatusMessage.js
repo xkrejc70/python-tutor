5162ee9d5a507d97689f4be07a10f731a055b2fc
hojzas <hojzas@seznam.cz>
2024-02-28 13:11:44 +0100

    Login feedback

--- client/src/components/StatusMessage.js
+++ client/src/components/StatusMessage.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function StatusMessage({ status }) {
-    return status && <p className={`status`}>{status}</p>;
+    let className = 'default';
+    
+    if (status === 'Settings saved') {
+        className = 'status status-success';
+    } else {
+        className = 'status';
+    }
+    return status && <p className={`status ${className}`}>{status}</p>;
 }
 
 export default StatusMessage;
