c5bba83ae8e1d7bc085e004077768c9ee5c1be65
hojzas <hojzas@seznam.cz>
2024-03-03 18:58:47 +0100

    Exercise for sorted (proj4)

--- client/src/pages/exercise/Proj4.js
+++ client/src/pages/exercise/Proj4.js
@@ -7,51 +7,52 @@ import scrabbleImg from 'assets/images/scrabble.png'
 
 function Proj4() {
     const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
-    const [codeLine, setCodeLine] = useState('_____1_____');
+    const [intersection, setIntersection] = useState('_____1_____');
+    const [sorted, setSorted] = useState('_____2_____');
 
     const onSidebarCollapsedChange = (collapsed) => {
         setSidebarCollapsed(collapsed);
     };
-
-    const handleNext = () => {
-        //TODO
-    };
     
-    const handlePrevious = () => {
-        //TODO
-    };
-
     const updateCode = (index) => {
-        setCodeLine('set(words).intersection(perms)');
+        switch (index) {
+            case 0:
+                setIntersection('set(words).intersection(perms)');
+                break;
+            case 1:
+                setSorted("sorted(valid_words, key=word_value)");
+                break;
+            default:
+                break;
+        }
     };
 
     const heading = "Task:";
     const description = `You are developing a Scrabble helper to assist players in finding valid words that can be formed using any subset of the letters they have on their rack.
     However, the challenge is that the Scrabble dictionary is extensive, containing tens of thousands of words.
-    The function needs to handle this vast dictionary efficiently to provide quick and relevant suggestions.`;
+    The function needs to handle this vast dictionary efficiently to provide quick and relevant suggestions.
+    Additionally, the Scrabble helper should prioritize words based on their total point value, enhancing the player's ability to choose words that yield the highest scores.`;
     const code = `def scrabble_helper(letters, words):
     """
-    Finds valid words that can be formed using any subset of the provided letters.
+    Efficiently identifies and sorts valid Scrabble words based on their total point value.
 
     Parameters:
     - letters (str): A string representing the available letters.
     - words (list): A list of words to check for validity.
 
     Returns:
-    - set: A set of valid words that can be formed using any subset of the available letters.
+    - set: A sorted list of valid words based on their total point value in Scrabble.
     """
     perms = all_permutations_substrings(letters)
-    return ${codeLine}`;
+    valid_words = ${intersection}
+    return ${sorted}`;
+
 
     return (
         <div>
             <Sidebar onCollapsedChange={onSidebarCollapsedChange} />
             <div className={"main-layout"} style={{ marginLeft: sidebarCollapsed ? "80px" : "250px" }}>
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <button onClick={handlePrevious}>Previous</button>
-                    <h1>Set operations</h1>
-                    <button onClick={handleNext}>Next</button>
-                </div>
+                <h1>Set operations</h1>
                 <br />
 
                 <div>
